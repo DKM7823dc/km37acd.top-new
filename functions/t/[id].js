7eb6d3e2982a259e2f5ebbc6b6ba3de2edd01bfb
DKM7823dc <mkyusds82a@hotmail.com>
2026-08-13 22:28:47 +0800

    Update [id].js

--- functions/t/[id].js
+++ functions/t/[id].js
@@ -1,1 +1,7 @@
-
+export async function onRequest(context) {
+  const id = String(context.params.id || '');
+  if (!/^[a-z0-9]{8}$/i.test(id)) {
+    return new Response('链接无效', { status: 404, headers: { 'Content-Type': 'text/plain; charset=utf-8' } });
+  }
+  return Response.redirect('https://url.yuhd320k.site/rd/' + id.toLowerCase(), 302);
+}
