7dd68eab138554952defb74c75c21eb8bf076c4c
DKM7823dc <mkyusds82a@hotmail.com>
2026-08-28 22:33:21 +0800

    Update [id].js

--- functions/t/[id].js
+++ functions/t/[id].js
@@ -3,5 +3,5 @@ export async function onRequest(context) {
   if (!/^[a-z0-9]{8}$/i.test(id)) {
     return new Response('链接无效', { status: 404, headers: { 'Content-Type': 'text/plain; charset=utf-8' } });
   }
-  return Response.redirect('https://url.yuhd320k.site/rd/' + id.toLowerCase(), 302);
+  return Response.redirect('https://api.km624da.site/rd/' + id.toLowerCase(), 302);
 }
